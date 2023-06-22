93ea7341abbb1a5884f806525370f838e535fea7
Twixii <lilkamal76@gmail.com>
2023-06-22 20:00:24 +0300

    starting section 2 of learning

---
+++ JS Course/The JavaScript language/Code quality/1. Debugging in the browser/start.js
@@ -0,0 +1,15 @@
+// let program_name = 'start.js';
+
+function start() {
+  // let name = prompt('Please, Enter your name.');
+  // let age = prompt('Please, Enter your age.');
+
+  // if(age >= 18) {
+  //   alert(`Hey, ${name} you age is allowed, you are adult enough to join us.`)
+  // } else {
+  //   alert('PLease grow up first.')
+  // }
+  for(let i = 0; i < 10; ++i) {
+    console.log(i);
+  }
+}
---
+++ JS Course/The JavaScript language/Code quality/2. Coding style/Bad style.js
@@ -0,0 +1,44 @@
+// Task1 for this lesson https://javascript.info/coding-style#bad-style
+
+/**
+ * What’s wrong with the code style below?
+
+function pow(x,n)
+{
+  let result=1;
+  for(let i=0;i<n;i++) {result*=x;}
+  return result;
+}
+
+let x=prompt("x?",''), n=prompt("n?",'')
+if (n<=0)
+{
+  alert(`Power ${n} is not supported, please enter an integer number greater than zero`);
+}
+else
+{
+  alert(pow(x,n))
+}
+Fix it.
+
+ */
+
+function pow(x, n) {
+  let result = 1;
+
+  for (let i = 0 ;i < n ;i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?",''),
+    n = prompt("n?",'');
+
+if (n <= 0) {
+  alert(`Power ${n} is not supported, 
+  please enter an integer number greater than zero`);
+} else {
+  alert(pow(x,n))
+}
