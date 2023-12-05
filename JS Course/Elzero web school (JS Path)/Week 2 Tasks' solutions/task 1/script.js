6ce25cf0c7f2deef365698e94d246b7f2091f532
Twixii <lilkamal76@gmail.com>
2023-12-05 21:11:10 +0200

    finished week2

---
+++ JS Course/Elzero web school (JS Path)/Week 2 Tasks' solutions/task 1/script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let numberOne = 10, numberTwo = 20;
+
+console.log(numberOne + "" + numberTwo);
+console.log(typeof (numberOne + "" + numberTwo))
+
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof `${numberOne}${numberTwo}`);
+
+console.log(numberOne + "\n" + numberTwo);
+console.log(`${numberOne}
+${numberTwo}`);
---
+++ JS Course/Elzero web school (JS Path)/Week 2 Tasks' solutions/task 3/script.js
@@ -0,0 +1,10 @@
+/*
+`I'm In
+\\
+Love \\ """ '''
+++ With ++
+\"""\"""
+""JavaScript""``
+*/
+
+console.log("`I\'m In\n\\\\\nLove \\\\ \"\"\" '''\n++ With ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``");
